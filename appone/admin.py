1212b50132bf8e2e7579051c716b0bc5355be9e7
mjjstockman <mjjstockman@gmail.com>
2022-04-18 14:25:55 +0000

    Add models

--- appone/admin.py
+++ appone/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import Gig, Song, User, Setlist
 
 # Register your models here.
+admin.site.register(Gig)
+admin.site.register(Song)
+admin.site.register(User)
+admin.site.register(Setlist)
--- appone/models.py
+++ appone/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Gig(models.Model):
+    city = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.city
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Setlist(models.Model):
+    gig = models.OneToOneField(Gig, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    song = models.ManyToManyField(Song)
+
+    def __str__(self):
+        return self.gig.city
