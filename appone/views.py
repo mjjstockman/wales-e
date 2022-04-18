95dd529e95e90c9ea2944edcf1897844de3f3e42
mjjstockman <mjjstockman@gmail.com>
2022-04-18 15:39:42 +0000

    Add create-setlist url and view

--- appone/views.py
+++ appone/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def create_setlist(request):
+    """ view to return index page """
+    return render(request, 'appone/create-setlist.html')
